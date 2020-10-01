44691011e1e8358d5e2d2d3cd82a87849dbec344
William Lundström <william.lundstrom2@learnet.se>
2020-10-01 13:38:04 +0200

    §1

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApp5
             DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
 
             Console.ReadKey();
+            Console.Write("hejsan");
             
         }
     }
